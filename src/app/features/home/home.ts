ca421095879526936be8c0027cd7149769aebc11
Athar Shaikh <atharshaikh7385@gmail.com>
2026-09-03 19:35:48 +0530

    Added Flight

--- src/app/features/home/home.ts
+++ src/app/features/home/home.ts
@@ -2,9 +2,13 @@ import { Component, inject } from '@angular/core';
 import { Navbar } from './components/navbar/navbar';
 import { FlightSearch } from './flight-search/flight-search';
 import { FlightCardList } from '../flight/flight-cards-list';
+import { FlightSortTabs } from '../flight/components/flight-sort-tabs/flight-sort-tabs';
+import { DayViewMiniGrid } from '../flight/components/day-view-mini-grid/day-view-mini-grid';
+import { FlightCard } from '../flight/components/flight-card/flight-card';
+import { StopsFilter } from '../flight/components/fliters/stops-filter/stops-filter';
 
 @Component({
-  imports: [Navbar, FlightSearch, FlightCardList],
+  imports: [Navbar, FlightSearch, FlightCardList, FlightSortTabs, DayViewMiniGrid, StopsFilter],
   selector: 'app-home',
   styleUrl: './home.css',
   templateUrl: './home.html',
